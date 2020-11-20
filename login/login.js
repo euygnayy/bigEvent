fce6e7889aa0e034a2cc1e9dc6c8b0b0713e5231
yangy <751514836@qq.com>
2020-11-20 11:05:53 +0800

    3login页面点击成功跳转页面

---
+++ login/login.js
@@ -0,0 +1,12 @@
+
+
+// -----------------------------切换窗口
+$("#goto-register").on("click",function() {
+    $("#login").hide();
+    $("#register").show();
+});
+
+$("#goto-login").on("click",function() {
+    $("#login").show();
+    $("#register").hide();
+});
